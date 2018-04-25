3acbfd8b2cb28f81a23b005df6b49be093763b36
Matthias Fischer <m@tthias.space>
2018-04-25 13:28:58 +0200

    added findChildNode

--- server/fupa-parser.js
+++ server/fupa-parser.js
@@ -7,48 +7,9 @@ module.exports = () => {
     // imports
     const https = require('https');
     const parse5 = require('parse5');
+    const findChildNode = require('find-child-node')();
 
     const fupaParser = {};
-
-    /**
-     * Searches an specific node by an attribute.
-     * @param {array} childNodes 
-     * @param {string} key 
-     * @param {string} value 
-     * @returns {object|null}
-     */
-    function findChildNodeByAttribute(childNodes, key, value) {
-        
-        var returnNode = null;
-        
-        childNodes.forEach((node) => {
-            if(node.attrs && node.attrs[0] 
-                && node.attrs[0].name == key 
-                && node.attrs[0].value == value) {
-                returnNode = node;
-            }
-        });
-
-        return returnNode;
-    }
-
-    /**
-     * Searched an specific node by class name.
-     * @param {string} childNodes 
-     * @param {string} className 
-     */
-    function findChildNodeByClass(childNodes, className) {
-        return findChildNodeByAttribute(childNodes, 'class', className);
-    }
-
-    /**
-     * Searched an specific node by id.
-     * @param {string} childNodes 
-     * @param {string} id
-     */
-    function findChildNodeById(childNodes, id) {
-        return findChildNodeByAttribute(childNodes, 'id', id);
-    }
 
     /**
      * Parse the html string to an array of team members
@@ -61,37 +22,37 @@ module.exports = () => {
             const htmlNode = htmlObj.childNodes[1];
             const bodyNode = htmlNode.childNodes[2];
 
-            const mainNode = findChildNodeByClass(bodyNode.childNodes, 'main');
+            const mainNode = findChildNode.byClass(bodyNode.childNodes, 'main');
             if(mainNode == null) {
                 reject('main node not available');
             }
 
-            const pageNode = findChildNodeByClass(mainNode.childNodes, 'page');
+            const pageNode = findChildNode.byClass(mainNode.childNodes, 'page');
             if(pageNode == null) {
                 reject('page node not available');
             }
 
-            const pageWrapperNode = findChildNodeById(pageNode.childNodes, 'ip_page_wrapper');
+            const pageWrapperNode = findChildNode.byId(pageNode.childNodes, 'ip_page_wrapper');
             if(pageWrapperNode == null) {
                 reject('page_wrapper node not available');
             }
 
-            const contentWrapperNode = findChildNodeById(pageWrapperNode.childNodes, 'ip_content_wrapper');
+            const contentWrapperNode = findChildNode.byId(pageWrapperNode.childNodes, 'ip_content_wrapper');
             if(contentWrapperNode == null) {
                 reject('content_wrapper node not available');
             }
 
-            const contentNode = findChildNodeByClass(contentWrapperNode.childNodes, 'content');
+            const contentNode = findChildNode.byClass(contentWrapperNode.childNodes, 'content');
             if(contentNode == null) {
                 reject('content node not available');
             }
 
-            const contentStandardNode = findChildNodeByClass(contentNode.childNodes, 'content_standard');
+            const contentStandardNode = findChildNode.byClass(contentNode.childNodes, 'content_standard');
             if(contentStandardNode == null) {
                 reject('content_standard node not available');
             }
 
-            const teamKaderContainerNode = findChildNodeByClass(contentStandardNode.childNodes, 'team_kader_container');
+            const teamKaderContainerNode = findChildNode.byClass(contentStandardNode.childNodes, 'team_kader_container');
             if(teamKaderContainerNode == null) {
                 reject('team_kader_container node not available');
             }
