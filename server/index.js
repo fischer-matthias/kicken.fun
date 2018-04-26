899a01dc336853e8a6d576053a58c620152c500f
Matthias Fischer <m@tthias.space>
2018-04-26 15:03:22 +0200

    changed directory structure

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var util = require('util')
 
-const fupaParser = require('./fupa-parser.js')();
+const fupaParser = require('./logic/fupa-parser.js')();
 
 fupaParser.getPlayersOfTeam('tus-bersenbrueck', 'm2')
     .then((result) => console.log(result))
