3b05b6e5895551f85c83f190ffb7852aaf35d8d1
Matthias Fischer <m@tthias.space>
2018-05-08 14:27:30 +0200

    fixed array[array] format from fupa.net

--- server/api/logic/club-crawler.js
+++ server/api/logic/club-crawler.js
@@ -9,6 +9,25 @@ module.exports = () => {
     const clubCrawler = {};
 
     /**
+     * Converts the fupa array[array] format to a single flat array.
+     * @param {*} response 
+     * @returns edited fupa response
+     */
+    function parseFupaResponse(response) {
+        var clubs = [];
+
+        for (var property in response) {
+            if (response.hasOwnProperty(property)) {
+                if(Array.isArray(response[property])) {
+                    clubs = clubs.concat(response[property]);
+                }
+            }
+        }
+
+        return clubs;
+    }
+
+    /**
      * Returns a list of clubs found by a specific search term.
      * @param {string} searchTerm 
      * @returns Promise<Array> of clubs
@@ -25,7 +44,7 @@ module.exports = () => {
                 
                 // The whole response has been received. Print out the result.
                 resp.on('end', () => {
-                    resolve(data);
+                    resolve(parseFupaResponse(JSON.parse(data)));
                 });
             
             }).on("error", (err) => {
